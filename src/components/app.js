264f49c000336de0d84d5d8a00127ecb6867ac75
Teddy Nadaskay <tnadaskay@icloud.com>
2021-01-05 17:30:27 -0700

    end of day

--- src/components/app.js
+++ src/components/app.js
@@ -3,15 +3,33 @@ import moment from "moment";
 
 import PortfolioContainer from "./Portfolio/portfolio-container";
 import NavigationContainer from "./navigation/Navigation-Container";
+import {
+  BrowserRouter as  Router,
+  Switch, 
+  Route
+} from 'react-router-dom';
+import Home from "./Pages/home";
+import About from "./Pages/about";
+
+
 
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path ="/" component={Home} />
+              <Route path ="/about-me" component={About} />
+            </Switch>
+          </div>
+        </Router>
+        
         <h1>Teddy Nadaskay</h1>
-        <h2>React Portfolio</h2>
 
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
