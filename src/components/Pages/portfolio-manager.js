3d248cdfc4d27e9bb0ed70611f50b90c3fc51e41
Teddy Nadaskay <tnadaskay@icloud.com>
2021-05-21 14:38:37 -0600

    added to portfolio form

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -1,42 +1,66 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
-export default class extends Component {
-    constructor() {
-        super();
-        this.state = {
-            portfolioItems: []
-        };
-    }
+export default class PortfolioManager extends Component {
+  constructor() {
+    super();
 
-    getPortfolioItems() {
-        axios.get("https://tnadaskay.devcamp.space/portfolio/portfolio_items", {
-            withCredentials: true
-        }).then(response => {
-            this.setState({
-                portfolioItems: [...response.data.portfolio_items]
-            });
-        }).catch(error => {
-            console.log("error in getPortfolioItems", error);
-        })
-    }
+    this.state = {
+      portfolioItems: []
+    };
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  }
 
-    render() {
-        return(
-            <div className="portfolio-manager-wrapper">
-                <div className="left-column">
-                    <h1>Portfolio Manager</h1>
-                </div>
+  handleSuccessfulFormSubmission(portfolioItem) {
+    // TODO
+    // update the portfolioItems state
+    // and add the portfolioItem to the list
+  }
 
-                <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems}/>
-                </div>
-            </div>
-        );
-    }
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError error", error);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
+        </div>
+
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Portfolio/portfolio-sidebar-list.js
+++ src/components/Portfolio/portfolio-sidebar-list.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-const PortfolioSidebarList = (props) => {
-    const portfolioList = props.data.map(portfolioItem => {
-        return (
-            <div>
-            <div>
-                <img src={portfolioItem.thumb_image_url} />
-            <div/>
-            </div>
-                <h1>{portfolioItem.name}</h1>
-                <h2>{portfolioItem.id}</h2>
-            </div>
-        )
-    })
 
-    return <div>{portfolioList}</div>
+const PortfolioSidebarList = props => {
+  const portfolioList = props.data.map(portfolioItem => {
+    return (
+      <div className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
+        </div>
+        <h1 className="title">{portfolioItem.name}</h1>
+        <h2>{portfolioItem.id}</h2>
+      </div>
+    );
+  });
+
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
-export default PortfolioSidebarList;  
+export default PortfolioSidebarList;
