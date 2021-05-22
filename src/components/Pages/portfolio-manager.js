0d2223c5420ca20e02189c908d10474550ff32e8
Teddy Nadaskay <tnadaskay@icloud.com>
2021-05-22 16:16:47 -0600

    test

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 import PortfolioForm from "../portfolio/portfolio-form";
-
+//test
 export default class PortfolioManager extends Component {
   constructor() {
     super();
